2fa5eaa4d8c66a2e0d5656566f9fd983759ba35f
pjaudiomv <34245618+pjaudiomv@users.noreply.github.com>
2023-10-20 13:06:23 -0400

    support for legacy comment syntax (#11)

--- nacc-wordpress-plugin.php
+++ nacc-wordpress-plugin.php
@@ -59,6 +59,7 @@ class NACC
             add_action('wp_enqueue_scripts', [$this, 'assets']);
             add_shortcode('nacc', [$this, 'setupShortcode']);
             add_filter('do_shortcode_tag', [$this, 'triggerAfterShortcodeLoaded'], 10, 3);
+            add_filter('the_content', [$this, 'naccContent']); // Support for legacy shortcodes
         }
     }
 
@@ -97,13 +98,17 @@ class NACC
             $language = !empty($atts['lang']) ? sanitize_text_field(strtolower($atts['lang'])) : get_option('nacc_language');
             $layout = !empty($atts['layout']) ? sanitize_text_field(strtolower($atts['layout'])) : get_option('nacc_layout');
             $special = !empty($atts['special']) ? sanitize_text_field(strtolower($atts['special'])) : get_option('nacc_special');
+            $siteURI = plugins_url('nacc2', __FILE__);
             $shortcodeAtts = [
                 'theme' => $theme,
                 'lang' => $language,
                 'layout' => $layout,
                 'special' => $special,
+                'siteURI' => $siteURI
             ];
-            add_action('wp_footer', [$this, 'renderKeytags']);
+            add_action('wp_footer', function () use ($shortcodeAtts) {
+                $this->renderKeytags($shortcodeAtts);
+            });
             return $output;
         }
         return $output;
@@ -117,15 +122,147 @@ class NACC
      *
      * @return void
      */
-    public function renderKeytags(): void
+    public function renderKeytags(array $args): void
     {
-        global $shortcodeAtts;
-        $args = $shortcodeAtts;
-        // Get the plugin's URI for scripts
-        $siteURI = '"' . plugins_url('nacc2', __FILE__) . '"';
-        // Add JavaScript inline script
-        wp_add_inline_script('nacc-js', 'var nacc = new NACC(\'nacc_container\', "' . $args['theme'] . '", "' . $args['lang'] . '", "' . $args['layout'] . '", "' . $args['special'] . '", ' . $siteURI . ');');
+        wp_add_inline_script('nacc-js', 'var nacc = new NACC(\'nacc_container\', "' . $args['theme'] . '", "' . $args['lang'] . '", "' . $args['layout'] . '", "' . $args['special'] . '", "' . $args['siteURI'] . '");');
     }
+
+    /** Begin Code to Support Legacy non standard shortcode syntax IE. `<!-- NACC -->` or `[[NACC]]` */
+
+    /**
+     * Process content and replace a shortcode with CleanTime Calculator HTML.
+     *
+     * @param string $theContent The content to process.
+     *
+     * @return string The modified content with CleanTime Calculator HTML.
+     */
+    public function naccContent(string $theContent): string
+    {
+        // Initialize default values for shortcode parameters
+        $theme = '';
+        $lang = 'en';
+        $layout = 'linear';
+        $showSpecial = 'true';
+        $siteURI = plugins_url('nacc2', __FILE__);
+
+        // Get the shortcode and decode it
+        $shortcode = html_entity_decode($this->getShortcode($theContent) ?? '');
+
+        // Check if a shortcode was found
+        if (!empty($shortcode)) {
+            // Initialize the CleanTime Calculator text
+            $ccText = '<div id="nacc_container"></div>' . "\n";
+            $ccText .= '<noscript>';
+            $ccText .= '<h1 style="text-align:center">JavaScript Required</h1>';
+            $ccText .= '<h2 style="text-align:center">Sadly, you must enable JavaScript on your browser in order to use this cleantime calculator.</h2>';
+
+            // Parse shortcode parameters
+            $shortcodeObj = explode(',', $shortcode);
+            if (count($shortcodeObj) > 0) {
+                $theme = trim(trim($shortcodeObj[0]), "'");
+
+                if (count($shortcodeObj) > 1) {
+                    $langTemp = trim(trim($shortcodeObj[1]), "'");
+                    if ($langTemp) {
+                        $lang = $langTemp;
+                    }
+                }
+
+                if (count($shortcodeObj) > 2) {
+                    $layout = trim(trim($shortcodeObj[2]), "'");
+                }
+
+                if (count($shortcodeObj) > 3) {
+                    $showSpecial = trim(trim($shortcodeObj[3]), "'");
+                }
+            }
+            $ccText .= '</noscript>' . "\n";
+            // Replace the shortcode in the content with CleanTime Calculator HTML
+            $theContent = $this->replaceShortcode($theContent, $ccText);
+        }
+
+        // Prepare parameters for rendering
+        $params = [
+            'theme' => $theme,
+            'layout' => $layout,
+            'lang' => $lang,
+            'special' => $showSpecial,
+            'siteURI' => $siteURI
+        ];
+
+        // Add an action to render legacy keytags in wp_footer
+        add_action('wp_footer', function () use ($params) {
+            $this->renderKeytags($params);
+        });
+
+        return $theContent;
+    }
+
+    /************************************************************************************/
+    /**
+     *   \brief This will parse the given text, to see if it contains the submitted code.    *
+     *                                                                                       *
+     *   The code can be contained in EITHER an HTML comment (<!--CODE-->), OR a double-[[]] *
+     *   notation.                                                                           *
+     *                                                                                       *
+     *   \returns A string, consisting of the new text.                                      *
+     ***************************************************************************************
+     * @param string $inTextToParse
+     * @param string $inReplacementText
+     * @return string|string[]|null
+     */
+    public function replaceShortcode(
+        string $inTextToParse,      ///< The text to search for shortcodes
+        string $inReplacementText    ///< The text we'll be replacing the shortcode with.
+    ) {
+        // Define the regular expressions for shortcode in HTML and brackets
+        $codeRegexHtml = "/(\<p[^\>]*?\>)?<!--\s?nacc\s?(\(.*?\))?\s?-->(\<\/p>)?/i";
+        $codeRegexBrackets = "/(\<p[^\>]*?\>)?\[\[\s?nacc\s?(\(.*?\))?\s?]](\<\/p>)?/i";
+
+        // Replace shortcodes in both formats with the replacement text
+        $ret = preg_replace($codeRegexHtml, $inReplacementText, $inTextToParse, 1);
+        $ret = preg_replace($codeRegexBrackets, $inReplacementText, $ret, 1);
+
+        return $ret;
+    }
+
+    /************************************************************************************/
+    /**
+     *   \brief This will parse the given text, to see if it contains the submitted code.    *
+     *                                                                                       *
+     *   The code can be contained in EITHER an HTML comment (<!--CODE-->), OR a double-[[]] *
+     *   notation.                                                                           *
+     *                                                                                       *
+     *   \returns Boolean true if the code is found (1 or more instances), OR an associative *
+     *   array of data that is associated with the code (anything within parentheses). Null  *
+     *   is returned if there is no shortcode detected.                                      *
+     ***************************************************************************************
+     * @param string $inTextToParse
+     * @return mixed
+     */
+    public function getShortcode(string $inTextToParse)
+    {
+        // Define the regular expressions for shortcode in HTML and brackets
+        $codeRegexHtml = "/<!--\s?nacc\s?(\(.*?\))?\s?-->/i";
+        $codeRegexBrackets = "/\[\[\s?nacc\s?(\(.*?\))?\s?]]/i";
+
+        // Initialize the result
+        $ret = null;
+
+        // Try to match the shortcode using both regular expressions
+        if (preg_match($codeRegexHtml, $inTextToParse, $matches) || preg_match($codeRegexBrackets, $inTextToParse, $matches)) {
+            // Check if there are any parameters and extract them
+            if (!empty($matches[1])) {
+                $ret = trim($matches[1], '()');
+            } else {
+                $ret = true; // No parameters, set to true
+            }
+        }
+
+        return $ret;
+    }
+
+    /** End Code to Support Legacy */
 
     /**
      * Enqueue plugin styles and scripts.
